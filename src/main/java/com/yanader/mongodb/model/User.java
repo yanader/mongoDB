8017a0454ed05313a5d0263d1de18724c4d01b3a
ste <mrbarrow@hotmail.co.uk>
2024-07-26 12:06:57 +0100

    fix: List<League> added to User

--- src/main/java/com/yanader/mongodb/model/User.java
+++ src/main/java/com/yanader/mongodb/model/User.java
@@ -2,6 +2,7 @@ package com.yanader.mongodb.model;
 
 import lombok.Data;
 import org.bson.types.ObjectId;
+import java.util.List;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -13,4 +14,5 @@ public class User {
     private ObjectId id;
     private String username;
     private String email;
+    private List<League> leagueMemberships;
 }
