4b3748595bce2224067005d05bea5414998cefce
ste <mrbarrow@hotmail.co.uk>
2024-07-26 11:50:35 +0100

    feat:User

---
+++ src/main/java/com/yanader/mongodb/model/User.java
@@ -0,0 +1,15 @@
+package com.yanader.mongodb.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collation = "users")
+public class User {
+
+    @Id
+    private String id;
+    private String username;
+    private String email;
+}
