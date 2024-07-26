dd63174d9439f993257610305edf257f769225f6
ste <mrbarrow@hotmail.co.uk>
2024-07-26 13:50:45 +0100

    feat: first user controller test

--- src/main/java/com/yanader/mongodb/controller/UserController.java
+++ src/main/java/com/yanader/mongodb/controller/UserController.java
@@ -1,20 +1,26 @@
 package com.yanader.mongodb.controller;
 
+import com.yanader.mongodb.model.User;
 import com.yanader.mongodb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/api/v1/photoleague/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
     @GetMapping
-    public String userControllerTest() {
-        return "User Controller";
+    public ResponseEntity<List<User>> userControllerTest() {
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
---
+++ src/test/java/com/yanader/mongodb/controller/UserControllerTest.java
@@ -0,0 +1,67 @@
+package com.yanader.mongodb.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.yanader.mongodb.model.User;
+import com.yanader.mongodb.service.UserServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserControllerTest {
+
+    @Mock
+    UserServiceImpl mockService;
+
+    @InjectMocks
+    UserController userController;
+
+    @Autowired
+    private MockMvc mockMvcController;
+
+    private ObjectMapper mapper;
+
+    List<User> users;
+
+    @BeforeEach
+    void setup() {
+        mockMvcController = MockMvcBuilders.standaloneSetup(userController).build();
+        mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        User userOne = new User("ste", "ste@email.com");
+        User userTwo = new User("Graton", "graton@email.com");
+        users = List.of(userOne, userTwo);
+    }
+
+    @Test
+    void getAllUsers() throws Exception {
+        when(mockService.getAllUsers()).thenReturn(users);
+
+        this.mockMvcController.perform(
+                MockMvcRequestBuilders.get("/api/v1/photoleague/users"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].username").value("ste"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].username").value("Graton"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].email").value("ste@email.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1].email").value("graton@email.com"));
+
+        verify(mockService, times(1)).getAllUsers();
+    }
+}
