bb1968f9213b2dec9dd5b7d805fec379fd0559b6
ste <mrbarrow@hotmail.co.uk>
2024-07-28 13:20:03 +0100

    feat: LeagueController

--- src/main/java/com/yanader/mongodb/controller/LeagueController.java
+++ src/main/java/com/yanader/mongodb/controller/LeagueController.java
@@ -1,20 +1,42 @@
 package com.yanader.mongodb.controller;
 
+import com.yanader.mongodb.model.League;
 import com.yanader.mongodb.service.LeagueService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/leagues")
+@RequestMapping("/api/v1/photoleague/leagues")
 public class LeagueController {
 
     @Autowired
     private LeagueService leagueService;
 
     @GetMapping
-    public String leagueControllerTest() {
-        return "League Controller";
+    public List<League> getLeagues() {
+        return leagueService.getLeagues();
     }
+
+    @PostMapping
+    public League addLeague(@RequestBody League league) {
+        return leagueService.addLeague(league);
+    }
+
+    @PutMapping("/{id}")
+    public League updateLeague(@PathVariable int id, @RequestBody League league) {
+        return leagueService.updateLeague(id, league);
+    }
+
+    @DeleteMapping("/{id}")
+    public League deleteLeague(@PathVariable int id) {
+        return leagueService.deleteLeague(id);
+    }
+
+    @GetMapping("/{id}")
+    public League getLeagueByName(@PathVariable String name) {
+        return leagueService.getLeagueByName(name);
+    }
+
 }
