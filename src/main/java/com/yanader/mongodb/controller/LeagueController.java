356f05e2ac1a11cbf1b7cac868d212d8029b69d6
ste <mrbarrow@hotmail.co.uk>
2024-07-29 17:27:00 +0100

    LeagueController -> ResponseEntities

--- src/main/java/com/yanader/mongodb/controller/LeagueController.java
+++ src/main/java/com/yanader/mongodb/controller/LeagueController.java
@@ -2,8 +2,11 @@ package com.yanader.mongodb.controller;
 
 import com.yanader.mongodb.model.League;
 import com.yanader.mongodb.service.LeagueService;
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -15,28 +18,49 @@ public class LeagueController {
     private LeagueService leagueService;
 
     @GetMapping
-    public List<League> getLeagues() {
-        return leagueService.getLeagues();
+    public ResponseEntity<List<League>> getLeagues() {
+        return new ResponseEntity<>(leagueService.getLeagues(), HttpStatus.OK);
     }
 
     @PostMapping
-    public League addLeague(@RequestBody League league) {
-        return leagueService.addLeague(league);
+    public ResponseEntity<League> addLeague(@RequestBody League league) {
+        League addedLeague = leagueService.addLeague(league);
+        if (addedLeague != null) {
+            return new ResponseEntity<>(addedLeague, HttpStatus.OK);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "League creation failed");
+        }
+
     }
 
     @PutMapping("/{id}")
-    public League updateLeague(@PathVariable int id, @RequestBody League league) {
-        return leagueService.updateLeague(id, league);
+    public ResponseEntity<League> updateLeague(@PathVariable int id, @RequestBody League league) {
+        League updatedLeague = leagueService.updateLeague(id, league);
+        if (updatedLeague != null) {
+            return new ResponseEntity<>(updatedLeague, HttpStatus.ACCEPTED);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "League update failed");
+        }
     }
 
     @DeleteMapping("/{id}")
-    public League deleteLeague(@PathVariable int id) {
-        return leagueService.deleteLeague(id);
+    public ResponseEntity<League> deleteLeague(@PathVariable int id) {
+        League deletedLeague = leagueService.deleteLeague(id);
+        if (deletedLeague != null) {
+            return new ResponseEntity<>(deletedLeague, HttpStatus.ACCEPTED);
+        } else {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "League deletion failed");
+        }
     }
 
     @GetMapping("/{name}")
-    public League getLeagueByName(@PathVariable String name) {
-        return leagueService.getLeagueByName(name);
+    public ResponseEntity<League> getLeagueByName(@PathVariable String name) {
+        League league = leagueService.getLeagueByName(name);
+        if (league != null) {
+            return new ResponseEntity<>(league, HttpStatus.OK);
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "League not found");
+        }
     }
 
 }
