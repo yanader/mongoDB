9b17c8afb7d6f61c764f6c79e116f87aae5e1baa
ste <mrbarrow@hotmail.co.uk>
2024-07-30 11:10:02 +0100

    refactor: fixing names

--- src/main/java/com/yanader/photoleague/PhotoLeagueApplication.java
+++ src/main/java/com/yanader/photoleague/PhotoLeagueApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MongodbApplication {
+public class PhotoLeagueApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MongodbApplication.class, args);
+		SpringApplication.run(PhotoLeagueApplication.class, args);
 	}
 
 }
--- src/test/java/com/yanader/photoleague/PhotoLeagueApplicationTests.java
+++ src/test/java/com/yanader/photoleague/PhotoLeagueApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MongodbApplicationTests {
+class PhotoLeagueApplicationTests {
 
 	@Test
 	void contextLoads() {
